d0434f9ebb65eb2ee8ce96e3bed175c9f02ce4f5
andregarcia0412 <andregarcia0412@gmail.com>
2026-03-05 13:34:59 -0300

    refactor: implement new register route

--- frontend/src/api/services/auth.service.ts
+++ frontend/src/api/services/auth.service.ts
@@ -7,7 +7,7 @@ import type {
 import { api } from "../api.client";
 
 export const loginService = async (
-  loginData: LoginDto
+  loginData: LoginDto,
 ): Promise<LoginResponseDto> => {
   const { email, password } = loginData;
   const response = await api.post<LoginResponseDto>("/auth/login", {
@@ -19,11 +19,11 @@ export const loginService = async (
 };
 
 export const registerService = async (
-  registerData: RegisterDto
+  registerData: RegisterDto,
 ): Promise<RegisterResponseDto> => {
   const { username, email, password } = registerData;
 
-  const response = await api.post<RegisterResponseDto>("/user", {
+  const response = await api.post<RegisterResponseDto>("/auth/register", {
     username: username,
     email: email,
     password: password,
