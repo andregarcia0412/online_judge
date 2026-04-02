c93801a3a2dd90d7265dfe376ba2a1069e365a87
andregarcia0412 <andregarcia0412@gmail.com>
2026-04-02 19:04:33 -0300

    :sparkles: feature: add NotFound page styling

--- frontend/src/pages/not-found/NotFound.tsx
+++ frontend/src/pages/not-found/NotFound.tsx
@@ -2,7 +2,7 @@ import "./style.not-found.css";
 
 export const NotFound = () => {
   return (
-    <div>
+    <div className="not-found-container">
       <h1>404 | Page not found</h1>
     </div>
   );
