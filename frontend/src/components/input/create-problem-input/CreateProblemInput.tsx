8cb2dddb19918f73517daa405a059907b83466c5
andregarcia0412 <andregarcia0412@gmail.com>
2026-04-12 00:04:55 -0300

    :sparkles: feature: add option for CreateProblemInput to be textarea too

--- frontend/src/components/input/create-problem-input/CreateProblemInput.tsx
+++ frontend/src/components/input/create-problem-input/CreateProblemInput.tsx
@@ -4,35 +4,50 @@ type CreateProblemInputProps = {
   text: string;
   setText: (text: string) => void;
   placeholder: string;
-  padding?: string;
   maxLength?: number;
   title: string;
+  type?: "input" | "textarea";
+  height?: number;
 };
 
 export const CreateProblemInput = ({
   text,
   setText,
   placeholder,
-  padding,
   maxLength,
   title,
+  height,
+  type = "input",
 }: CreateProblemInputProps) => {
   return (
     <div>
       <div className="create-input-row">
         <p>{title}</p>
 
-        <input
-          className="create-problem-input"
-          style={{ padding }}
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder={placeholder}
-          maxLength={maxLength}
-        />
-        {maxLength && <p className="create-input-length">
-          {text.length}/{maxLength} characters
-        </p>}
+        {type == "input" ? (
+          <input
+            style={{ height }}
+            className="create-problem-input"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder={placeholder}
+            maxLength={maxLength}
+          />
+        ) : (
+          <textarea
+            style={{ height }}
+            className="create-problem-input"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder={placeholder}
+            maxLength={maxLength}
+          />
+        )}
+        {maxLength && (
+          <p className="create-input-length">
+            {text.length}/{maxLength} characters
+          </p>
+        )}
       </div>
     </div>
   );
