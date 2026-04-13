662623958145615548a791fcf6349c71ca28c22f
andregarcia0412 <andregarcia0412@gmail.com>
2026-04-12 23:16:14 -0300

    :sparkles: feature: add readonly option for CategoryBadge component

--- frontend/src/components/card/category-badge/CategoryBadge.tsx
+++ frontend/src/components/card/category-badge/CategoryBadge.tsx
@@ -1,24 +1,36 @@
-import React from "react";
-import "./style.category-badge.css";
 import CheckCircleWhite from "../../../assets/check_circle_white.svg";
+import "./style.category-badge.css";
 
 type CategoryBadgeProps = {
   title: string;
-  onClick: () => void;
+  onClick?: () => void;
+  onSelectedClick?: () => void;
+  highlighted?: boolean;
+  readonly?: boolean;
 };
 
-export const CategoryBadge = ({ title, onClick }: CategoryBadgeProps) => {
-  const [isHighlighted, setHighlited] = React.useState<boolean>(false);
+export const CategoryBadge = ({
+  title,
+  onClick,
+  onSelectedClick,
+  highlighted,
+  readonly,
+}: CategoryBadgeProps) => {
+  const isMarked = Boolean(highlighted);
+
   return (
     <div
-      className={`category-badge ${isHighlighted && "marked"}`}
+      className={`category-badge ${isMarked && "marked"} ${readonly && "readonly"}`}
       onClick={() => {
-        setHighlited(!isHighlighted);
-        onClick();
+        if (isMarked && onSelectedClick) {
+          onSelectedClick();
+          return;
+        }
+        if (onClick) onClick();
       }}
     >
       <div className="category-badge-body">
-        {isHighlighted && <img src={CheckCircleWhite} />}
+        {isMarked && <img src={CheckCircleWhite} />}
         <p>{title}</p>
       </div>
     </div>
