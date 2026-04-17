e8170f1cdbc23c26e8d7547d976ed205bdf66270
andregarcia0412 <andregarcia0412@gmail.com>
2026-04-16 23:26:20 -0300

    :recycle: refactor: rewrite Divider component to tailwind

--- frontend/src/components/divider/Divider.tsx
+++ frontend/src/components/divider/Divider.tsx
@@ -1,13 +1,13 @@
-import "./style.divider.css";
-
 interface DividerProps {
   text: string;
 }
 
-export const Divider = (props: DividerProps) => {
+export const Divider = ({ text }: DividerProps) => {
   return (
-    <div className="divider">
-      <span>{props.text}</span>
+    <div className="flex items-center text-center my-5 text-[#666] w-full">
+      <div className="flex-1 border-b border-[#666]" />
+      {text ? <span className="mx-4">{text}</span> : null}
+      <div className="flex-1 border-b border-[#666]" />
     </div>
   );
 };
