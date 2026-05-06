85bed968e5567ce5c0f61f5f9c0efb39b1459352
andregarcia0412 <andregarcia0412@gmail.com>
2026-05-06 00:25:04 -0300

    :recycle: refactor: refactor HomeHeader to use tailwind instead of pure css

--- frontend/src/components/home-header/HomeHeader.tsx
+++ frontend/src/components/home-header/HomeHeader.tsx
@@ -1,6 +1,5 @@
 import { Logo } from "../logo/Logo";
 import { QuickSearch } from "../input/quick-search/QuickSearch";
-import "./style.home-header.css";
 import { useNavigate } from "react-router-dom";
 import Target from "../../assets/target.svg";
 
@@ -17,17 +16,23 @@ export const HomeHeader = ({
 }: HomeHeaderProps) => {
   const navigate = useNavigate();
   return (
-    <header className="home-header">
-      <div className="logo-title" onClick={() => navigate("/")}>
+    <header className="flex items-center justify-between w-full py-3 px-8 bg-black/70 border-t border-b border-[#374151]">
+      <div
+        className="flex justify-center items-center gap-2 cursor-pointer"
+        onClick={() => navigate("/")}
+      >
         <Logo />
-        <div className="header-title">
-          <h1>
-            Code<span>Judge</span>
+        <div className="text-white text-base">
+          <h1 className="text-3xl">
+            Code
+            <span className="bg-linear-to-r from-[#7d8bff] to-[#3b82f6] bg-clip-text text-transparent">
+              Judge
+            </span>
           </h1>
         </div>
       </div>
 
-      <div className="header-item">
+      <div className="flex gap-2 items-center text-white opacity-80">
         <img src={Target} />
         <p>Problems</p>
       </div>
