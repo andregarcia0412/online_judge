bde9b0c4c513f9bdb22a1a7f9bd9f32ba599ab86
andregarcia0412 <andregarcia0412@gmail.com>
2026-05-16 02:05:14 -0300

    :recycle: refactor: StatCard component to use tailwindcss

--- frontend/src/components/card/info-card/StatCard.tsx
+++ frontend/src/components/card/info-card/StatCard.tsx
@@ -1,10 +1,8 @@
-import "./style.info-card.css";
-
 type StatCardProps = {
   title: string;
   value: string;
   description: string;
-  color: string;
+  color: "green" | "blue" | "orange" | "pink";
   icon: string;
 };
 
@@ -15,15 +13,24 @@ export const StatCard = ({
   color,
   icon,
 }: StatCardProps) => {
+  const statCardColors = {
+    green: "bg-[rgba(34,197,94,0.1)]",
+    blue: "bg-[rgba(59,130,246,0.1)]",
+    orange: "bg-[rgba(249,115,22,0.1)]",
+    pink: "bg-[rgba(236,72,153,0.1)]",
+  };
+
   return (
-    <div className="stat-card">
-      <div className={`stat-card-icon ${color}`}>
-        <img src={icon} />
+    <div className="group flex justify-center items-start flex-col gap-3 bg-[rgba(17,24,39,0.4)] text-white border border-[#30363d] w-full min-w-34 h-37.5 transition-all duration-100 ease-in py-1 px-5 rounded-xl hover:border-[#8b5cf6]">
+      <div
+        className={`flex items-center p-2 rounded-md max-w-fit transition-transform duration-100 ease-in group-hover:scale-105 ${statCardColors[color]}`}
+      >
+        <img className="w-5 h-5" src={icon} />
       </div>
-      <div className="stat-card-title">
-        <p className="stat-card-subtitle">{title}</p>
-        <h1 className="stat-card-value">{value}</h1>
-        <p className="stat-card-subtitle">{description}</p>
+      <div className="flex flex-col justify-center gap-0.75">
+        <p className="opacity-60 text-xs">{title}</p>
+        <h1 className="text-2xl">{value}</h1>
+        <p className="opacity-60 text-xs">{description}</p>
       </div>
     </div>
   );
