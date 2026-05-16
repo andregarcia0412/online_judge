9c87ea578ff2bb146adc5825fde5779fa8d53c99
andregarcia0412 <andregarcia0412@gmail.com>
2026-05-16 01:34:17 -0300

    :recycle: refactor: refactor CreateProblemCard to tailwind

--- frontend/src/components/card/create-problem-card/CreateProblemCard.tsx
+++ frontend/src/components/card/create-problem-card/CreateProblemCard.tsx
@@ -1,5 +1,4 @@
 import type React from "react";
-import "./style.create-problem-card.css";
 
 type CreateProblemCardProps = {
   title: string;
@@ -13,10 +12,10 @@ export const CreateProblemCard = ({
   children,
 }: CreateProblemCardProps) => {
   return (
-    <div className="create-card">
-      <div className="create-card-title">
-        <h2>{title}</h2>
-        {subtitle && <p>{subtitle}</p>}
+    <div className="relative flex flex-col w-full bg-[rgba(17,24,39,0.4)] border border-[#30363d] text-white py-6 px-8 rounded-xl gap-6">
+      <div className="flex flex-col gap-2">
+        <h2 className="font-normal">{title}</h2>
+        {subtitle && <p className="opacity-70">{subtitle}</p>}
       </div>
 
       {children}
