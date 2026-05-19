af32ab399e812984316630e9a91460c113cafd4e
andregarcia0412 <andregarcia0412@gmail.com>
2026-05-19 17:38:32 -0300

    :recycle: refactor: better error treatment when model is offline

--- llm-gateway/src/modules/model/provider/ollama_provider.py
+++ llm-gateway/src/modules/model/provider/ollama_provider.py
@@ -1,4 +1,5 @@
 from ollama import AsyncClient
+from fastapi import HTTPException
 from src.modules.model.provider.model_provider_port import ModelProviderPort
 
 class OllamaProvider(ModelProviderPort):
@@ -7,14 +8,19 @@ class OllamaProvider(ModelProviderPort):
         self.client = AsyncClient()
     
     async def send_chat_message(self, message: str) -> str:
-        return (await self.client.chat(
-            model="online_judge_model",
-            messages=[
-                {
-                    "role": "user",
-                    "content": message
-                }
-            ]
-        ))["message"]["content"]
-
+        try:
+            return (await self.client.chat(
+                model="online_judge_model",
+                messages=[
+                    {
+                        "role": "user",
+                        "content": message
+                    }
+                ]
+            ))["message"]["content"]
+        except (ConnectionError):
+            raise HTTPException(
+                status_code=503,
+                detail="Model offline"
+            )
         
