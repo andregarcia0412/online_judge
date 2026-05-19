8e7d4bf00d441e1b6efb9a32125a5bd75dc55abe
andregarcia0412 <andregarcia0412@gmail.com>
2026-05-19 18:03:19 -0300

    :bug: fix: adjust ollama provider and docker compose for llm to work on docker env

--- llm-gateway/src/modules/model/provider/ollama_provider.py
+++ llm-gateway/src/modules/model/provider/ollama_provider.py
@@ -1,11 +1,13 @@
 from ollama import AsyncClient
 from fastapi import HTTPException
+import os
 from src.modules.model.provider.model_provider_port import ModelProviderPort
 
 class OllamaProvider(ModelProviderPort):
     
     def __init__(self):
-        self.client = AsyncClient()
+        host = os.getenv("OLLAMA_HOST", "http://localhost:11434")
+        self.client = AsyncClient(host=host)
     
     async def send_chat_message(self, message: str) -> str:
         try:
