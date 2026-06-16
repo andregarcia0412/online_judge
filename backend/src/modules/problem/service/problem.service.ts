f351bde1272f1b0230f027c225caaffbf9bb1214
andregarcia0412 <andregarcia0412@gmail.com>
2026-06-16 20:36:15 -0300

    :recycle: refactor: adjust problem service to use use cases patterns

--- backend/src/modules/problem/service/problem.service.ts
+++ backend/src/modules/problem/service/problem.service.ts
@@ -1,22 +1,18 @@
-import {
-  ConflictException,
-  Inject,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { DeleteResult, UpdateResult } from 'typeorm';
 import { ReturnCategoryDto } from '../dto/category/return-category.dto';
 import { CreateProblemDto } from '../dto/problem/create-problem.dto';
 import { ReturnProblemDto } from '../dto/problem/return-problem.dto';
 import { UpdateProblemDto } from '../dto/problem/update-problem.dto';
 import { ReturnTestCaseDto } from '../dto/test-case/return-test-case.dto';
-import { Problem } from '../entities/problem.entity';
 import { TestCase } from '../entities/test-case.entity';
 import { CreateProblemUseCase } from '../use-case/problem/create.use-case';
 import { FindAllProblemUseCase } from '../use-case/problem/find-all.use-case';
 import { FindProblemByIdUseCase } from '../use-case/problem/find-by-id.use-case';
 import { FindProblemByTitleUseCase } from '../use-case/problem/find-by-title.use-case';
 import { FindAllTestCasesByProblemIdUseCase } from '../use-case/problem/find-test-cases-by-id.use-case';
+import { RemoveProblemUseCase } from '../use-case/problem/remove.use-case';
+import { UpdateProblemUseCase } from '../use-case/problem/update.use-case';
 
 @Injectable()
 export class ProblemService {
@@ -31,6 +27,10 @@ export class ProblemService {
     private readonly findProblemByTitleUseCase: FindProblemByTitleUseCase,
     @Inject(FindAllTestCasesByProblemIdUseCase)
     private readonly findAllTestCasesByProblemIdUseCase: FindAllTestCasesByProblemIdUseCase,
+    @Inject(UpdateProblemUseCase)
+    private readonly updateProblemUseCase: UpdateProblemUseCase,
+    @Inject(RemoveProblemUseCase)
+    private readonly removeProblemUseCase: RemoveProblemUseCase,
   ) {}
 
   async create(createProblemDto: CreateProblemDto): Promise<ReturnProblemDto> {
@@ -86,10 +86,10 @@ export class ProblemService {
     id: number,
     updateProblemDto: UpdateProblemDto,
   ): Promise<UpdateResult> {
-    return await this.problemRepository.updateById(id, updateProblemDto);
+    return await this.updateProblemUseCase.execute(id, updateProblemDto);
   }
 
   async remove(id: number): Promise<DeleteResult> {
-    return await this.problemRepository.delete(id);
+    return await this.removeProblemUseCase.execute(id);
   }
 }
