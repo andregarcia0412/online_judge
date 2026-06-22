2e3ae113613f16e39fe60373188d2ca1437617fb
andregarcia0412 <andregarcia0412@gmail.com>
2026-06-22 20:22:43 -0300

    :recycle: refactor: remove unused import on user repository

--- backend/src/modules/user/repository/user.repository.ts
+++ backend/src/modules/user/repository/user.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { DeleteResult, EntityManager, Repository } from 'typeorm';
+import { EntityManager, Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 import { UserRepositoryPort } from '../interface/user.repository.port';
 
