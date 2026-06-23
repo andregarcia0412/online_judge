41792a492958a15b64e71613658deccbfc04156d
andregarcia0412 <andregarcia0412@gmail.com>
2026-06-23 15:50:01 -0300

    :memo: refactor: adjust update dtos to use swagger typing

--- backend/src/modules/problem/dto/test-case/update-test-case.dto.ts
+++ backend/src/modules/problem/dto/test-case/update-test-case.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateTestCaseDto } from './create-test-case.dto';
 
 export class UpdateTestCaseDto extends PartialType(CreateTestCaseDto) {}
