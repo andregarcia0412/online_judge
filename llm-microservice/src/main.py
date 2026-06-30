bdd403df15eeb7ffe8667bc660a48e0caa9d3554
andregarcia0412 <andregarcia0412@gmail.com>
2026-06-30 14:40:57 -0300

    :sparkles: feature: protect routes with x-api-key

--- llm-microservice/src/main.py
+++ llm-microservice/src/main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from src.modules.model.model_controller import model_router
 from src.modules.system.system_controller import system_router
+from src.shared.security.verify_key import verify_api_key
 
 app = FastAPI(
     title="Online Judge LLM Gateway",
-    version="1.0.0"
+    version="1.0.0",
+    dependencies=[Depends(verify_api_key)]
 )
 
 app.include_router(model_router)
