6cfc26cc5cf512f2fbadd743cbb849a280f2f523
andregarcia0412 <andregarcia0412@gmail.com>
2026-06-30 13:25:27 -0300

    :sparkles: feature: create new modelfiles and update entrypoint.sh to download new models

--- ollama/entrypoint.sh
+++ ollama/entrypoint.sh
@@ -6,7 +6,8 @@ pid=$!
 
 sleep 5
 
-ollama create online_judge_model -f /root/online_judge_modelfile
+ollama create online_judge_chat_model -f /root/online_judge_chat_modelfile
+ollama create online_judge_evaluation_model -f /root/online_judge_evaluation_modelfile
 
 echo "✅ Ollama ready!"
 
