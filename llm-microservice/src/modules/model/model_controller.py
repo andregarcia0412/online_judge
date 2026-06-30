a7b7438642cf859c3d6e3694fd8ee9eec24c0f59
andregarcia0412 <andregarcia0412@gmail.com>
2026-06-30 13:52:20 -0300

    :sparkles: feature: add response dto to both message endpoints

--- llm-microservice/src/modules/model/model_controller.py
+++ llm-microservice/src/modules/model/model_controller.py
@@ -3,15 +3,16 @@ from src.modules.model.model_service import ModelService
 from src.modules.model.dto.create_chat_message_dto import CreateChatMessageDto
 from src.modules.model.dto.create_evaluation_message_dto import CreateEvaluationMessageDto
 from src.modules.model.provider.ollama_provider import OllamaProvider
+from src.modules.model.dto.response_dto import ResponseDto
 
 ollama_provider = OllamaProvider()
 model_router = APIRouter(prefix="/model", tags=["model"])
 model_service = ModelService(ollama_provider)
 
 @model_router.post("/chat")
-async def generate_chat_message(create_chat_message_dto: CreateChatMessageDto):
+async def generate_chat_message(create_chat_message_dto: CreateChatMessageDto) -> ResponseDto:
     return await model_service.send_chat_message_to_llm(create_chat_message_dto)
 
 @model_router.post("/evaluation")
-async def generate_evaluation_message(create_evaluation_message_dto: CreateEvaluationMessageDto):
+async def generate_evaluation_message(create_evaluation_message_dto: CreateEvaluationMessageDto) -> ResponseDto:
     return await model_service.send_evaluation_message_to_llm(create_evaluation_message_dto)
--- llm-microservice/src/modules/model/model_service.py
+++ llm-microservice/src/modules/model/model_service.py
@@ -1,17 +1,18 @@
 from src.modules.model.dto.create_evaluation_message_dto import CreateEvaluationMessageDto
 from src.modules.model.dto.create_chat_message_dto import CreateChatMessageDto
 from src.modules.model.provider.model_provider_port import ModelProviderPort
+from src.modules.model.dto.response_dto import ResponseDto
 
 class ModelService:    
     def __init__(self, model_provider: ModelProviderPort):
         self.model_provider = model_provider
 
-    async def send_chat_message_to_llm(self, create_chat_message_dto: CreateChatMessageDto):
+    async def send_chat_message_to_llm(self, create_chat_message_dto: CreateChatMessageDto) -> ResponseDto:
         message = f"{create_chat_message_dto.message}\nMy code in: {create_chat_message_dto.language}\n{create_chat_message_dto.code}"
 
-        return await self.model_provider.send_chat_message(message)
+        return ResponseDto(message=(await self.model_provider.send_chat_message(message)))
     
-    async def send_evaluation_message_to_llm(self, create_evaluation_message_dto: CreateEvaluationMessageDto):
+    async def send_evaluation_message_to_llm(self, create_evaluation_message_dto: CreateEvaluationMessageDto) -> ResponseDto:
         message = f"ANALYZE_COMPLEXITY {create_evaluation_message_dto.code}"
 
-        return await self.model_provider.send_evaluation_message(message)
+        return ResponseDto(message=(await self.model_provider.send_evaluation_message(message)))
