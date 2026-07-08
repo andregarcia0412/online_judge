2250b84475370a6b0a711c55ce88300ffbd80c1f
andregarcia0412 <andregarcia0412@gmail.com>
2026-07-08 13:12:31 -0300

    :sparkles: feature: add more status to status enum

--- backend/src/modules/submission/enum/submission-status.ts
+++ backend/src/modules/submission/enum/submission-status.ts
@@ -1,5 +1,10 @@
 export enum StatusEnum {
-  REJECTED = 'rejected',
+  COMPILATION_ERROR = 'compilation error',
+  RUNTIME_ERROR = 'runtime error',
+  TIME_LIMIT_EXCEEDED = 'time limit exceeded',
+  WRONG_ANSWER = 'wrong answer',
+  PRESENTATION_ERROR = 'presentation error',
+  SUBMISSION_ERROR = 'submission error',
   PENDING = 'pending',
   ACCEPTED = 'accepted',
 }
