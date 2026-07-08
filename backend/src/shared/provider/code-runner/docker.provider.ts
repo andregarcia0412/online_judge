0a372bad49affb1667dc7c5a6895f6f75dc6d36c
andregarcia0412 <andregarcia0412@gmail.com>
2026-07-08 13:12:57 -0300

    :sparkles: feature: make docker provider and execute code dto return compilation error or time limit exceeded

--- backend/src/shared/provider/code-runner/docker.provider.ts
+++ backend/src/shared/provider/code-runner/docker.provider.ts
@@ -9,6 +9,7 @@ import { ExecuteCodeDto } from 'src/shared/provider/code-runner/dto/execute-code
 import { TarPackProviderPort } from '../tar-pack/tar-pack.provider.port';
 import { CodeRunnerProviderPort } from './code-runner.provider.port';
 import { LANGUAGES } from './infra/languages/languages';
+import { StatusEnum } from 'src/modules/submission/enum/submission-status';
 
 @Injectable()
 export class DockerProvider implements CodeRunnerProviderPort {
@@ -125,6 +126,7 @@ export class DockerProvider implements CodeRunnerProviderPort {
             0,
             true,
             0,
+            StatusEnum.COMPILATION_ERROR,
           );
         }
       }
@@ -175,6 +177,7 @@ export class DockerProvider implements CodeRunnerProviderPort {
           executionTime,
           true,
           memoryUsageMB,
+          StatusEnum.TIME_LIMIT_EXCEEDED,
         );
       }
 
