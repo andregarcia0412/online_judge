e60886a49f1bcb7770243efb892a387a2de56aef
andregarcia0412 <andregarcia0412@gmail.com>
2026-07-26 23:25:41 -0300

    :recycle: refactor: migrate logo from css to tailwind

--- frontend/src/components/logo/Logo.tsx
+++ frontend/src/components/logo/Logo.tsx
@@ -3,8 +3,8 @@ import Code from "../../assets/code.svg";
 
 export const Logo = () => {
   return (
-    <div className="logo-container">
-      <img draggable={false} src={Code} />
+    <div className="flex justify-center items-center h-8 w-8 bg-[#8b5cf6] rounded-lg">
+      <img draggable={false} src={Code} className="h-4.5 w-4.5" />
     </div>
   );
 };
