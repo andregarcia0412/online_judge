8692967a36592fc860b5ab2e7a9b7ad89d236d9d
andregarcia0412 <andregarcia0412@gmail.com>
2026-07-27 23:55:45 -0300

    :recycle: refactor: migrate not found to tailwindcss

--- frontend/src/pages/not-found/NotFound.tsx
+++ frontend/src/pages/not-found/NotFound.tsx
@@ -2,8 +2,8 @@ import "./style.not-found.css";
 
 export const NotFound = () => {
   return (
-    <div className="not-found-container">
-      <h1>404 | Page not found</h1>
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-white">404 | Page not found</h1>
     </div>
   );
 };
