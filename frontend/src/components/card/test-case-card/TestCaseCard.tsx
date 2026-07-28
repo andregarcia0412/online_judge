fc82d56359c9119be2afe7a3c352aa32c357ed80
andregarcia0412 <andregarcia0412@gmail.com>
2026-07-27 23:14:15 -0300

    :recycle: refactor: migrate test case card to tailwind css

--- frontend/src/components/card/test-case-card/TestCaseCard.tsx
+++ frontend/src/components/card/test-case-card/TestCaseCard.tsx
@@ -1,5 +1,4 @@
 import closeSmall from "../../../assets/close_small.svg";
-import "./style.test-case-card.css";
 
 type TestCaseCardProps = {
   index: number;
@@ -14,25 +13,32 @@ export const TestCaseCard = ({
   output,
   onDelete,
 }: TestCaseCardProps) => {
+  const inputStyles = "bg-black border border-[#30363d] p-3";
+  const inputParagraphStyles = "text-[#4ade80] font-md opacity-100";
+
   return (
-    <div className="test-case-card">
-      <div className="test-case-card-title">
+    <div className="flex flex-col p-5 border border-[#374151] rounded-xl gap-3">
+      <div className="flex items-center justify-between">
         <p>Test Case #{index}</p>
-        <img src={closeSmall} onClick={onDelete} />
+        <img
+          className="cursor-pointer transition-all duration-150 ease-in bg-transparent p-0.5 hover:bg-[rgba(239,68,68,0.1)] hover:rounded-md"
+          src={closeSmall}
+          onClick={onDelete}
+        />
       </div>
 
-      <div className="test-case-card-input-row">
-        <div className="test-case-card-input-container">
-          <p>Input</p>
-          <div className="test-case-card-input">
-            <p>{input}</p>
+      <div className="flex gap-6 *:flex-1">
+        <div className="flex flex-col gap-2">
+          <p className="text-sm opacity-70">Input</p>
+          <div className={inputStyles}>
+            <p className={inputParagraphStyles}>{input}</p>
           </div>
         </div>
 
-        <div className="test-case-card-input-container">
-          <p>Output</p>
-          <div className="test-case-card-input">
-            <p>{output}</p>
+        <div className="flex flex-col gap-2">
+          <p className="text-sm opacity-70">Output</p>
+          <div className={inputStyles}>
+            <p className={inputParagraphStyles}>{output}</p>
           </div>
         </div>
       </div>
