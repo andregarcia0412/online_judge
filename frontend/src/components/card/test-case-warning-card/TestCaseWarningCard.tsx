28f1708ca194d11aeabc872796a14230335ab044
andregarcia0412 <andregarcia0412@gmail.com>
2026-07-27 23:36:12 -0300

    :recycle: refactor: migrate test case warning card to tailwindcss

--- frontend/src/components/card/test-case-warning-card/TestCaseWarningCard.tsx
+++ frontend/src/components/card/test-case-warning-card/TestCaseWarningCard.tsx
@@ -1,17 +1,19 @@
 import error from "../../../assets/error.svg";
-import "./style.warning-card.css";
 
 export const TestCaseWarningCard = () => {
   return (
-    <div className="test-case-warning-card">
-      <div className="test-case-warning-card-title">
+    <div className="flex flex-col bg-[rgba(59,130,246,0.1)] border border-[#1f2937] border-l-4 border-l-[#3b82f6] rounded-tr-xl rounded-br-xl p-5 gap-2">
+      <div className="flex items-center gap-2">
         <img src={error} />
-        <p>About Test Cases</p>
+        <p className="text-[#93c5fd]">About Test Cases</p>
       </div>
-      <p className="test-case-warning-card-description">
+      <p className="text-sm ml-8">
         If you want the test case to have a line break at the end, be sure to
-        add a <code className="test-case-warning-card-code">{"\\n"}</code> at
-        the end of the output
+        add a{" "}
+        <code className="bg-[#1f2937] text-[#e5e7eb] py-0.5 px-1.5 rounded-sm">
+          {"\\n"}
+        </code>{" "}
+        at the end of the output
       </p>
     </div>
   );
