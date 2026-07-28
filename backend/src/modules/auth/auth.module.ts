6555af2b4b1bdc27f1d0974c6eeeb4b2b58ab223
andregarcia0412 <andregarcia0412@gmail.com>
2026-07-28 18:06:34 -0300

    :sparkles: feature: password reset and email sending logic on auth module

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,22 +1,45 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from 'src/modules/user/user.module';
 import { BcryptProvider } from 'src/shared/provider/hash/bcrypt.provider';
 import { HashProviderPort } from 'src/shared/provider/hash/hash.provider.port';
+import { EmailSenderProvider } from 'src/shared/provider/mail/email-sender.provider';
+import { EmailSenderProviderPort } from 'src/shared/provider/mail/email-sender.provider.port';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './common/jwt.strategy';
+import { PasswordResetCode } from './entities/password-reset-code.entity';
 import { AuthServicePort } from './interface/auth.service.port';
+import { PasswordResetCodeRepositoryPort } from './interface/password-code.repository.port';
 import { JwtProvider } from './provider/jwt.provider';
 import { JwtProviderPort } from './provider/jwt.provider.port';
+import { PasswordResetCodeRepository } from './repository/password-code.repository';
+import { FindActivePasswordResetCodeUseCase } from './use-case/find-active.use-case';
+import { GeneratePasswordResetCodeUseCase } from './use-case/generate-code.use-case';
+import { IncrementAttemptsUseCase } from './use-case/increment-attempts.use-case';
+import { InvalidateAllUseCase } from './use-case/invalidate-all.use-case';
+import { MarkAsUsedUseCase } from './use-case/mark-used.use-case';
+import { ValidateCodeUseCase } from './use-case/validate-code.use-case';
 
 @Module({
-  imports: [UserModule],
+  imports: [TypeOrmModule.forFeature([PasswordResetCode]), UserModule],
   controllers: [AuthController],
   providers: [
     { provide: AuthServicePort, useClass: AuthService },
     { provide: JwtProviderPort, useClass: JwtProvider },
     { provide: HashProviderPort, useClass: BcryptProvider },
+    { provide: EmailSenderProviderPort, useClass: EmailSenderProvider },
+    {
+      provide: PasswordResetCodeRepositoryPort,
+      useClass: PasswordResetCodeRepository,
+    },
     JwtStrategy,
+    FindActivePasswordResetCodeUseCase,
+    GeneratePasswordResetCodeUseCase,
+    IncrementAttemptsUseCase,
+    InvalidateAllUseCase,
+    MarkAsUsedUseCase,
+    ValidateCodeUseCase,
   ],
 })
 export class AuthModule {}
--- backend/src/modules/auth/interface/auth.service.port.ts
+++ backend/src/modules/auth/interface/auth.service.port.ts
@@ -2,11 +2,17 @@ import { CreateUserDto } from 'src/modules/user/dto/create-user.dto';
 import { AuthResponseDto } from '../dto/auth-response.dto';
 import { LoginDto } from '../dto/login.dto';
 import { RefreshTokenDto } from '../dto/refresh-token.dto';
+import { PasswordResetRequestDto } from '../dto/password-reset-request.dto';
+import { ResetPasswordDto } from '../dto/reset-password.dto';
 
 export interface AuthServicePort {
   login(loginDto: LoginDto): Promise<AuthResponseDto>;
   refresh(refreshToken: RefreshTokenDto): Promise<AuthResponseDto>;
   register(createUserDto: CreateUserDto): Promise<AuthResponseDto>;
+  requestPasswordReset(
+    passwordResetDto: PasswordResetRequestDto,
+  ): Promise<void>;
+  resetPassword(resetPasswordDto: ResetPasswordDto): Promise<void>;
 }
 
 export const AuthServicePort = Symbol('AuthServicePort');
