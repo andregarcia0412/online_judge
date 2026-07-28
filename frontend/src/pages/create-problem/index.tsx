8b7ea0cda7a753f0a87c225cb4efee0581414901
andregarcia0412 <andregarcia0412@gmail.com>
2026-07-28 10:47:36 -0300

    :recycle: refactor: migrate create problem page to tailwindcss

--- frontend/src/pages/create-problem/index.tsx
+++ frontend/src/pages/create-problem/index.tsx
@@ -159,13 +159,17 @@ export const CreateProblem = () => {
     }
   };
 
+  const inputsRowStyle = "flex justify-center gap-6 *:flex-1";
+
   return (
     <div>
       <HomeHeader setText={() => {}} handleSearch={() => {}} text="" />
-      <div className="create-container">
-        <div className="create-title">
+      <div className="create-container flex flex-col gap-8 w-full h-screen p-6">
+        <div className="flex flex-col text-white gap-2">
           <h1>Create New Problem</h1>
-          <p>Fill in the fields below to create a new programming problem</p>
+          <p className="opacity-70">
+            Fill in the fields below to create a new programming problem
+          </p>
         </div>
 
         <ConfigProvider
@@ -200,7 +204,7 @@ export const CreateProblem = () => {
             title="Problem Title"
           />
 
-          <div className="inputs-row">
+          <div className={inputsRowStyle}>
             <CreateProblemInput
               placeholder="Your name or username"
               text={createProblemForm.author}
@@ -225,8 +229,8 @@ export const CreateProblem = () => {
               title="Points"
             />
           </div>
-          <div className="create-selector">
-            <p>Difficulty</p>
+          <div className="flex flex-col justify-center gap-2">
+            <p className="font-sm">Difficulty</p>
             <ConfigProvider
               theme={{
                 algorithm: theme.darkAlgorithm,
@@ -268,12 +272,15 @@ export const CreateProblem = () => {
             </ConfigProvider>
           </div>
 
-          <div className="create-categories-wrapper">
-            <div className="create-categories-title">
-              <p>Categories</p>
+          <div className="flex flex-col gap-2">
+            <div className="relative flex items-center gap-2">
+              <p className="text-sm">Categories</p>
 
-              <button onClick={() => setVisiblePopover((prev) => !prev)}>
-                <img src={add} />
+              <button
+                className="flex justify-center items-center border-none rounded-md cursor-pointer p-1 bg-linear-to-r from-[#9333ea] to-[#2563eb] hover:opacity-70"
+                onClick={() => setVisiblePopover((prev) => !prev)}
+              >
+                <img className="h-5 w-5" src={add} />
               </button>
 
               {
@@ -313,9 +320,11 @@ export const CreateProblem = () => {
                 </Popover>
               }
             </div>
-            <div className="create-categories">
+            <div className="bg-black border border-[#30363d] p-3 flex gap-3">
               {createProblemForm.categories.length <= 0 ? (
-                <p>No selected categories</p>
+                <p className="text-white opacity-50 text-sm">
+                  No selected categories
+                </p>
               ) : (
                 createProblemForm.categories.map((category) => {
                   return (
@@ -379,7 +388,7 @@ export const CreateProblem = () => {
         </CreateProblemCard>
 
         <CreateProblemCard title="Examples">
-          <div className="inputs-row">
+          <div className={inputsRowStyle}>
             <CreateProblemInput
               placeholder="5\n10\n"
               setText={(text) =>
@@ -416,7 +425,7 @@ export const CreateProblem = () => {
           title="Test Cases"
           subtitle="Add the test cases that will be used to validate the submissions"
         >
-          <div className="create-test-case-card-inner">
+          <div className="flex flex-col gap-6">
             <TestCaseWarningCard />
 
             {testCases.length > 0 &&
@@ -433,9 +442,9 @@ export const CreateProblem = () => {
                 );
               })}
 
-            <div className="create-test-case-container">
+            <div className="flex flex-col p-6 border-2 border-dashed border-[#30363d] rounded-xl gap-4">
               <p>Add new test case</p>
-              <div className="inputs-row">
+              <div className={inputsRowStyle}>
                 <CreateProblemInput
                   text={createProblemForm.testCaseInput}
                   setText={(text) =>
@@ -478,7 +487,7 @@ export const CreateProblem = () => {
           </div>
         </CreateProblemCard>
 
-        <div className="create-button-row">
+        <div className="flex gap-4 pb-12">
           <Button
             background="rgba(0, 0, 0, 0.5)"
             loading={false}
