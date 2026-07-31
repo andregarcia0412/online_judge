4297759534bf26d0066e01696278a7bae32ff4bb
andregarcia0412 <andregarcia0412@gmail.com>
2026-07-31 14:45:58 -0300

    :sparkles: feature: update env variables with s3 ones

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -39,6 +39,11 @@ import { CacheModule } from './shared/provider/cache/cache.module';
         REDIS_HOST: Joi.string().required(),
         REDIS_PORT: Joi.number().port().required(),
 
+        AWS_REGION: Joi.string().required(),
+        AWS_ACCESS_KEY_ID: Joi.string().required(),
+        AWS_SECRET_ACCESS_KEY: Joi.string().required(),
+        S3_BUCKET: Joi.string().required(),
+
         JWT_ACCESS_SECRET: Joi.string().required(),
         JWT_REFRESH_SECRET: Joi.string().required(),
         JWT_ACCESS_EXPIRATION_TIME: Joi.string().required(),
