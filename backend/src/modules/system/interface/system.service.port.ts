3742c706f8c698ad21ed75e20ad8012d81641761
andregarcia0412 <andregarcia0412@gmail.com>
2026-08-25 17:20:35 -0300

    :fire: refactor: delete old system module

--- backend/src/modules/system/interface/system.service.port.ts
+++
@@ -1,9 +0,0 @@
-import { HealthDto } from '../dto/health.dto';
-import { InfoDto } from '../dto/info.dto';
-
-export interface SystemServicePort {
-  health(): HealthDto;
-  info(): InfoDto;
-}
-
-export const SystemServicePort = Symbol('SystemServicePort');
