1291753743cc3bf8c8c91e679ec3f33f49ee0579
andregarcia0412 <andregarcia0412@gmail.com>
2026-09-02 13:20:22 -0300

    :recycle: refactor: adjust create problem use case to use transactional

--- backend/src/modules/problem/use-case/problem/create.use-case.ts
+++ backend/src/modules/problem/use-case/problem/create.use-case.ts
@@ -1,5 +1,5 @@
+import { Transactional } from '@nestjs-cls/transactional';
 import { ConflictException, Inject, Injectable } from '@nestjs/common';
-import { DataSource } from 'typeorm';
 import { CreateProblemDto } from '../../dto/problem/create-problem.dto';
 import { Problem } from '../../entities/problem.entity';
 import { ProblemRepositoryPort } from '../../interface/repository/problem.repository.port';
@@ -9,28 +9,18 @@ export class CreateProblemUseCase {
   constructor(
     @Inject(ProblemRepositoryPort)
     private readonly problemRepository: ProblemRepositoryPort,
-    private readonly dataSource: DataSource,
   ) {}
 
+  @Transactional()
   async execute(createProblemDto: CreateProblemDto): Promise<Problem> {
-    return await this.dataSource.transaction(async (manager) => {
-      if (
-        await this.problemRepository.findByTitle(
-          createProblemDto.title,
-          manager,
-        )
-      ) {
-        throw new ConflictException('A problem with this title already exists');
-      }
+    if (await this.problemRepository.findByTitle(createProblemDto.title)) {
+      throw new ConflictException('A problem with this title already exists');
+    }
 
-      return await this.problemRepository.createAndSave(
-        {
-          ...createProblemDto,
-          categories: createProblemDto.category,
-          testCases: createProblemDto.testCases,
-        },
-        manager,
-      );
+    return await this.problemRepository.createAndSave({
+      ...createProblemDto,
+      categories: createProblemDto.category,
+      testCases: createProblemDto.testCases,
     });
   }
 }
